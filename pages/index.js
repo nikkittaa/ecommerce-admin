081996a31ac87301f42024ea06b53ac786ab1095
Nikita <nikitanb04@gmail.com>
2024-07-03 18:26:33 +0530

    basic dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,23 @@
-import Nav from "@/components/Nav";
-import { useSession, signIn, signOut } from "next-auth/react"
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession()
+  const {data : session } = useSession();
 
-  if(!session){
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className = 'text-center w-full'>
-          <button onClick = {() => signIn('google')}className = 'bg-white p-2 px-4 rounded-lg'>Login with Google</button>
+  if(!session) return;
+  return(
+    <Layout>
+      <div className = 'text-blue-900 flex justify-between'>
+        <h2>
+        Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className = 'flex bg-gray-300 text-black gap-1 rounded-lg overflow-hidden'>
+        <img src = {session?.user?.image} alt = 'profile image' className = 'w-8 h-8'/>
+        <span className = 'py-1 px-2'>
+        {session?.user?.name}
+        </span>
         </div>
       </div>
-    );
-  }
-
-  return(
-    <div className = 'bg-blue-900 min-h-screen'>
-      <Nav/>
-      <div>Logged in {session.user.email}</div>
-    </div>
-    
+    </Layout>
   );
-  
 }
