3d7a612370d17a21555e1a3635ad09526ee803f5
Nikita <nikitanb04@gmail.com>
2024-07-03 19:32:09 +0530

    added new product page

--- pages/products.js
+++ pages/products.js
@@ -1,9 +1,12 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Products(){
     return(
         <Layout>
-            products page here
+            <Link className = 'bg-blue-900 text-white rounded-md p-2' href = {'/products/new'}>
+                Add new products
+            </Link>
         </Layout>
     );
 }
