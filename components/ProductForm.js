982443234e55dffed51fe619b7b6dace6ce29c08
Nikita <nikitanb04@gmail.com>
2024-07-05 18:48:58 +0530

    adding images

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -5,11 +5,14 @@ import { useState } from "react";
 
 export default function ProductForm({_id, title : exTitle,
     description: exDescription, 
-    price: exPrice}){
+    price: exPrice,
+    images
+    }){
 
     const [title, setTitle] = useState(exTitle || '');
     const [description, setDescription] = useState(exDescription || '');
     const [ price, setPrice] = useState(exPrice || '');
+
     const [ goToProducts, setGoToProducts] = useState(false);
     const router = useRouter();
     const {enqueueSnackbar} = useSnackbar();
@@ -33,6 +36,22 @@ export default function ProductForm({_id, title : exTitle,
     if(goToProducts){
         router.push('/products');
     }
+
+    async function uploadImages(ev){
+        const files = ev.target?.files;
+        if(files?.length > 0){
+            const data = new FormData();
+            for(const file of files){
+                data.append('file', file);
+            }
+            const res = await fetch('/api/upload', {
+                method: 'POST',
+                body: data,
+            });
+            console.log(res);
+
+        }
+    }
     return (
         
             <form onSubmit = {saveProduct}>
@@ -42,6 +61,19 @@ export default function ProductForm({_id, title : exTitle,
                 value = {title}
                 onChange = {ev => setTitle(ev.target.value)}
             />
+            <label>Photos</label>
+            <div className = 'mb-2'>
+                <label className = 'w-24 h-24 rounded-xl flex flex-col items-center justify-center text-sm text-gray-600 bg-gray-300 cursor-pointer'>
+                <input type = 'file' onChange = {uploadImages} className = 'hidden'/>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+                </svg>Upload
+                
+                </label>
+                {!images?.length && (
+                    <div>No photos uploaded</div>
+                )}
+            </div>
             <label>Description</label>
             <textarea placeholder = "Description"
                 value = {description}
