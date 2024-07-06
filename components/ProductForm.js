77f2da98f3fa4c0bfa507d85d0046d3bc5484d6e
Nikita <nikitanb04@gmail.com>
2024-07-06 19:34:00 +0530

    attached aws s3 bucket to add images

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -44,11 +44,8 @@ export default function ProductForm({_id, title : exTitle,
             for(const file of files){
                 data.append('file', file);
             }
-            const res = await fetch('/api/upload', {
-                method: 'POST',
-                body: data,
-            });
-            console.log(res);
+            const res = await axios.post('/api/upload', data);
+            console.log(res.data);
 
         }
     }
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,5 +1,9 @@
 import multiparty from 'multiparty';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import fs from 'fs';
+import mime from 'mime-types';
 
+const bucketName = 'nikita-next-ecommerce';
 export default async function handle(req, res){
     const form = new multiparty.Form();
     const {fields, files} = await new Promise((resolve, reject) => {
@@ -9,8 +13,34 @@ export default async function handle(req, res){
            
         });
     });
+
+   
+
+    const client = new S3Client({
+        region: 'eu-north-1',
+        credentials: {
+            accessKeyId: process.env.S3_ACCESS_KEY,
+            secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+        },
+    });
     
-    res.json('ok');
+    const links = [];
+    for(const file of files.file){
+        const ext = file.originalFilename.split('.').pop();
+        const newFilename =  Date.now() + '.'+ext;
+        await client.send(new PutObjectCommand({
+            Bucket: bucketName,
+            Key: newFilename,
+            Body: fs.readFileSync(file.path),
+            ACL: 'public-read',
+            ContentType: mime.lookup(file.path),
+        }));
+
+        const link = `https://${bucketName}.s3.eu-north-1.amazonaws.com/${newFilename}`;
+        links.push(link);
+    }
+   
+    return res.json(links);
     
 }
 
